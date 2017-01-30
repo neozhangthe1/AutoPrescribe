706de4c547a95a383722b5ba1a77de08d3e8c036
neozhangthe1 <stack@live.cn>
2017-01-29 22:58:12 -0500

    processor

--- models/processor.py
+++ models/processor.py
@@ -16,14 +16,16 @@ class Processor:
         self.dev_data = self.load(config.dev_pkl)
         self.source_vocab = self.load(config.source_vocab_pkl)
         self.target_vocab = self.load(config.target_vocab_pkl)
+
+        self.config = config
+        self.idx_to_source_token, self.idx_to_target_token = self.build_index()
+
         self.source_vocab_size = len(self.source_vocab)
         self.target_vocab_size = len(self.target_vocab)
         print('train_size', len(self.train_data))
         print('dev_size', len(self.dev_data))
         print('source vocab size', self.source_vocab_size)
         print('target vocab size', self.target_vocab_size)
-        self.config = config
-        self.idx_to_source_token, self.idx_to_target_token = self.build_index()
 
         random.seed(config.model_seed)
 
@@ -69,19 +71,19 @@ class Processor:
         source_inputs, target_inputs, target_outputs, source_mask_inputs, target_mask_inputs, refs = get_data_structure()
 
         i = 0
-        UNK_INDEX = self.get_char_index('UNK')
+        # UNK_INDEX = self.get_char_index('UNK')
         for pair in data:
             target_text, source_text = tuple(pair)
 
             max_target_len = min(len(target_text) + 1, config.target_len)
-            target_inputs[i, 0] = self.get_char_index('START')
+            target_inputs[i, 0] = self.get_char_index('START', False)
             if max_target_len == len(target_text) + 1:
-                target_outputs[i, max_target_len - 1] = self.get_char_index('END')
+                target_outputs[i, max_target_len - 1] = self.get_char_index('END', False)
             else:
                 target_outputs[i, max_target_len - 1] = self.get_char_index(target_text[max_target_len - 1])
             for j in range(max_target_len - 1):
-                target_inputs[i, j + 1] = self.get_char_index(target_text[j])
-                target_outputs[i, j] = self.get_char_index(target_text[j])
+                target_inputs[i, j + 1] = self.get_char_index(target_text[j], False)
+                target_outputs[i, j] = self.get_char_index(target_text[j], False)
             target_mask_inputs[i, : max_target_len] = 1.0
 
             for j in range(len(source_text)):
@@ -173,6 +175,9 @@ class Processor:
         return source_inputs, target_inputs, target_outputs, source_mask_inputs, target_mask_inputs, map_inputs
 
     def build_index(self):
+        self.target_vocab["START"] = max(self.target_vocab.items()) + 1
+        self.target_vocab["END"] = max(self.target_vocab.items()) + 1
+        self.target_vocab["UNK"] = max(self.target_vocab.items()) + 1
         idx_to_source_token = dd(int)
         idx_to_target_token = dd(int)
         for token in self.source_vocab:
