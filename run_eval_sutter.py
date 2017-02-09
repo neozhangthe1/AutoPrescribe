ea89cf9b5b9f08fd429cfa9126f4456b3650ab1a
neozhangthe1 <stack@live.cn>
2017-02-09 10:17:33 -0500

    eval sutter

--- run_eval_sutter.py
+++ run_eval_sutter.py
@@ -1,6 +1,6 @@
 from models.processor import Processor
 from models.coverage import CoverageModel
-from exp.coverage import config_mimic as config
+from exp.coverage import config_sutter as config
 from utils.data import dump
 
 config = config.get_config()
